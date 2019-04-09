eb65d6f14d314f1a21eeec854daddb814fd23c38
Ning Yi <ialeonard@ucdavis.edu>
2019-04-09 16:05:09 -0700

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,14 +6,23 @@ module.exports = {
 };
 
 function succeed(item) {
+  
+  if (item.enhancement !== 20)
+  item.enhancement += 1;
+
   return { ...item };
 }
 
 function fail(item) {
+  if(item.durability > 4)
+     item.enhancement < 15 ? item.durability -= 5 : item.durability -= 10;
+  if (item.enhancement > 16) 
+    item.enhancement > 0 ? item.enhancement-= 1: item.enhancement -= 0;
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,52 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+let user = {
+    name: 'Gilbert',
+    enhancement: 20,
+    durability: 97
+}
+
+describe('enhancer', () => {
+    describe('repair()', () => {
+        it('should restore durability', () => {
+            expect(enhancer.repair(user).durability).toBe(100);
+        })
+    }); // describe repair
+
+
+    describe('succeed()', () => {
+        
+        it('should increment enhancement', () => {
+            enhancementLevel = user.enhancement;
+            console.log(enhancementLevel);
+            if ( enhancementLevel !== 20)
+                expect(enhancer.succeed(user).enhancement).toBe(enhancementLevel+1)
+            else 
+                expect(enhancer.succeed(user).enhancement).toBe(20);
+        })
+
+    }); // describe success 
+
+    describe('fail()', () => {
+        it('should decrement attributes', () => {
+            let item = {
+                name: 'Wilbur',
+                enhancement: 4,
+                durability: 7
+            }
+            durability = item.durability;
+            enhancement = item.enhancement;
+            console.log(durability, enhancement);
+                if( enhancement < 15)
+                    expect(enhancer.fail(item).durability).toBe(durability - 5);
+                else 
+                    expect(enhancer.fail(item).durability).toBe(durability - 10);
+                if( enhancement > 16)
+                    expect(enhancer.fail(item).enhancement).toBe(enhancement - 1)
+                if( enhancement === 0)
+                    expect(enhancer.fail(item).enhancement).toBe(0);
+        })
+    }); //describe fail
+
+}); // end outer describe
